72b3b6039356cc3bdb8d9162b1efd033c331789c
Himalaya Pal <himalayapal123@gmail.com>
2022-06-21 17:06:22 +0545

    User object destructing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import First from "./components/FirstComponents";
 function App() {
   return (
     <div>
-      <First/>
+      <First 
+      title={'New component'}
+      name={'John'}
+      age={25}
+      subject={['React', 'Angular', 'Vue']}
+      location={{city:'New York', country:'USA'}}
+      isStudent = {false}
+       />
         
     </div>
   );
--- src/components/FirstComponents.js
+++ src/components/FirstComponents.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const First = () => {
+const First = ({title,name,age,gender,subject,location,isStudent}) => {
   return (
     <div>
         <h1>I am first component</h1>
+        <h2>{title}</h2>
+        <h2>{name}</h2>
+        <h2>{age}</h2>
+        <h2>{subject}</h2>
+        {isStudent===true ?<button type="button">I am first</button>: 'none' }
+        {subject.map(item => <li>{item}</li>)}
+        <h3>Location : {location.city} and {location.country}</h3>
     </div>
   )
 }
